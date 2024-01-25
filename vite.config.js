35e8b4cc6a1a3c4473471c96f7873489134b3bb3
Flávia Reis <flaviarster@gmail.com>
2024-01-25 18:21:37 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/profile-card-component/",
+  base: "/profile-card-component",
   plugins: [react()],
 })
