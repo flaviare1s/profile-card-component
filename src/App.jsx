007810facda1b3437e38a11ce1b3bb6aab465449
Flávia Reis <flaviarster@gmail.com>
2024-01-25 13:55:24 -0300

    Making styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,37 @@ export function App() {
 
   return (
      <div>
-        Victor Crest
-        26
-        London
-
-        80K
-        Followers
-
-        803K
-        Likes
-
-        1.4K
-        Photos 
+        <main>
+          <div className="container">
+            <div className="card">
+              <div className='image'>
+                <img src="/public/image-victor.jpg" alt="Victor's photo" />
+              </div>
+              <div className="card-content">
+                <h1>Victor Crest <span>26</span></h1>
+                <p>London</p>
+                <div>
+                  <div>
+                    80K
+                    Followers
+                  </div>
+                  <div>
+                    803K
+                    Likes
+                  </div>
+                  <div>
+                    1.4K
+                    Photos
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <div className='attribution'>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/flaviare1s">Flávia Reis</a>.
+        </div>
     </div>
   )
 }
